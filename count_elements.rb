de7ce55a15b325b9819dd2f717840e183b908988
ruthnewman <ruthnewman@gmail.com>
2018-12-11 18:31:15 +0000

    Automatically backed up by Learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,14 @@
 def count_elements(array)
   # code goes here
+  new_hash = {}
+  array.each do |element|
+    count = 1
+    if !new_hash.has_key?(element)
+      new_hash[element] = 1
+    elsif new_hash.has_key?(element)
+      count += 1
+      new_hash[element] = count
+    end 
+  end 
 end
  
